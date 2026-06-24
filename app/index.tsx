d5655041c5e992f7ea7e9e193fdbef78bed79d1f
krennarjashari <krenar.a.jashari@gmail.com>
2026-06-24 23:37:41 +0200

    created Search componen UI

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { View, ScrollView, TouchableOpacity, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../components/Header';
+import Search from '../components/Search';
 
 function HomeScreen() {
   return (
     <View style={styles.webViewportCenter}>
-        <SafeAreaView className='w-full max-w-[430px] h-full bg-[#0F2038] relative'>
+        <SafeAreaView style={styles.phoneContainer} edges={['right', 'bottom', 'left']}>
 
-            <ScrollView contentContainerStyle={{flexGrow:1}}>
+            <ScrollView contentContainerStyle={styles.scrollLayout} bounces={false}>
                 <Header/>
+                <Search/>
             </ScrollView>
 
         </SafeAreaView>
@@ -28,30 +30,12 @@ const styles = StyleSheet.create({
   },
   phoneContainer: {
     width: '100%',
-    maxWidth: 430,
+    maxWidth: 430, 
     height: '100%',
     backgroundColor: '#0F2038', 
-    position: 'relative',
-    overflow: 'hidden',
   },
   scrollLayout: {
     flexGrow: 1,
-    paddingBottom: 120, 
-  },
-  floatingActionButton: {
-    position: 'absolute',
-    bottom: 36,
-    right: 24,
-    backgroundColor: '#FFFFFF', 
-    width: 56,
-    height: 56,
-    borderRadius: 28,
-    alignItems: 'center',
-    justifyContent: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 4.65,
-    elevation: 8,
+    paddingBottom: 40,
   },
 });
---
+++ components/Search.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { View, TextInput, StyleSheet } from 'react-native';
+import { SearchIcon } from 'lucide-react-native';
+
+function Search() {
+  return (
+    <View style={styles.container}>
+        <SearchIcon color="rgba(255, 255, 255, 0.4)" size={18} style={styles.icon}/>
+
+        <TextInput
+        style={styles.input}
+        placeholder='Search by title..'
+        placeholderTextColor="#8E8E93"
+        editable={true}
+        />
+    </View>
+  )
+}
+
+export default Search
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    backgroundColor: 'rgba(23, 23, 23, 0.8)',
+    marginHorizontal: 24,
+    marginTop: 20,
+    borderRadius: 12,
+    alignItems: 'center',
+    paddingHorizontal: 12,
+    height: 44,
+    borderWidth: 1,
+    borderColor: '#262626',
+  },
+  icon: {
+    marginRight: 8,
+  },
+  input: {
+    flex: 1,
+    color: '#FFFFFF',
+    fontSize: 16,
+    height: '100%',
+  },
+});
